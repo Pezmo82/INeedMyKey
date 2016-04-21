5d889466128fb0f0488a96faa06570e8e6851da8
Ted Beaney <tedbeaney@hotmail.co.uk>
2016-04-21 19:08:45 +0100

    routes update re contact us

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
 
-  get "contact" => "static_pages#contact"
+  
   get "account" => "static_pages#account"
+  get "contact" => "static_pages#contact"
+  match '/contacts', to: 'contacts#new', via: 'get'
+  resources "contacts", only: [:new, :create]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
