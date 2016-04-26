d5f3817ac0412ca026eecc0a8e1381b997078b48
AyporosV <personal@unveiledgnosis.com>
2016-04-26 19:54:12 +0100

    Added keys controller.

---
+++ app/controllers/keys_controller.rb
@@ -0,0 +1,38 @@
+class KeysController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def show
+  end
+
+  def new
+
+    @key = Key.new
+
+  end
+
+  def create
+
+    @key = Key.new(key_params)
+
+    if @key.save
+      flash[:notice] = 'Key added successfully'
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  end
+
+  def delete
+  end
+
+  private
+
+  def key_params
+
+    params.require(:key).permit(:name)
+
+  end
+
+end
