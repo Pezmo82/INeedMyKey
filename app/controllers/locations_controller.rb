941f530e616df15cd0712592f6d67666c1c8c8df
Ted Beaney <tedbeaney@hotmail.co.uk>
2016-05-05 19:19:33 +0100

    locations controller amended

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,26 @@
 class LocationsController < ApplicationController
+
+  def new
+
+    @location = Location.new
+
+  end
+
+  def create
+    @location = Location.new(params[:location])
+      flash[:notice] = "Location lodged."
+      redirect_to location_path
+    else
+      render :action => 'new'
+    end
+  end
+	
+	def delete
+
+	end
+
+	def show
+
+	end
+	
 end
