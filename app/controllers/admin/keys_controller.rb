3a121f037e5af63a1365dd08dfddff1b5f1acaa9
Perry Marquer <pezmo82@gmail.com>
2016-05-12 19:41:26 +0100

    Clean up

--- app/controllers/admin/keys_controller.rb
+++ app/controllers/admin/keys_controller.rb
@@ -1,12 +1,10 @@
 class Admin::KeysController < ApplicationController
 
-  before_action :authenticate_user!
-  before_action :check_rank
+  	before_action :authenticate_user!
+  	before_action :check_rank
 
-  def index
-
-    @Keys = Key.all
-
-  end
+  	def index
+    	@Keys = Key.all
+  	end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,24 @@
 class ApplicationController < ActionController::Base
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
 
-  protected
+    protected
 
-  def configure_permitted_parameters
-
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :email_confirmation, :mobile_number, :mobile_number_confirmation, :password, :password_confirmation) }
-
-  end
-
-  def check_rank
-
-    if current_user.rank_id < 1
-
-      redirect_to root_path
-
-      return false
-
-    else
-
-      return true
-
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :email_confirmation, :mobile_number, :mobile_number_confirmation, :password, :password_confirmation) }
     end
 
-  end
+    def check_rank
+        if current_user.rank_id < 1
+            redirect_to root_path
+            return false
+        else
+            return true
+        end
+    end
 
 end
