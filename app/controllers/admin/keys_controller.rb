91cf4cd6b9c79c904123f60818b82b3793d8ab86
Ted Beaney <tedbeaney@hotmail.co.uk>
2016-05-17 19:15:48 +0100

    Edit Super Admin - users key location edit function

--- app/controllers/admin/keys_controller.rb
+++ app/controllers/admin/keys_controller.rb
@@ -7,4 +7,22 @@ class Admin::KeysController < ApplicationController
     	@Keys = Key.all
   	end
 
+    def edit
+        @key = Key.find(params[:id])
+    end
+
+    def update
+        @key = Key.find(params[:id])
+        
+        if @key.update_attributes(key_params)
+            flash[:notice] = "Key Location Updated."
+            redirect_to admin_keys_path
+        end
+    end
+
+        private
+
+            def key_params
+                params.require(:key).permit(:location)
+            end
 end
