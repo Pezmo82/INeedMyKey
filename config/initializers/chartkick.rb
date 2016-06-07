72fd7246408cd594c8c0382b2d184756656a213c
Perry Marquer <pezmo82@gmail.com>
2016-06-07 22:39:34 +0100

    Chart Kick styling

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,6 +1,8 @@
-Chartkick.options = {
-	width: "100%",
-	height: "300px",
-  	colors: ["#604A9D", "#555"],
-  	library: {backgroundColor: "#E6E6E6"}
+Chartkick.options = { 
+	library: { 
+		width: "100%", 
+		height: "300px", 
+		colors: ["#604A9D"] 
+	    backgroundColor: ['fill': '#E6E6E6', 'opacity': 100]
+	 }	
 }
