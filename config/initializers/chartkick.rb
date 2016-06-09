8deefa2869ab20ca9dadce30269117867722e6dc
Perry Marquer <pezmo82@gmail.com>
2016-06-09 13:56:27 +0100

    Fixed syntax error

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -2,7 +2,7 @@ Chartkick.options = {
 	library: { 
 		width: "100%", 
 		height: "300px", 
-		colors: ["#604A9D"] 
+		colors: ["#604A9D"],
 	    backgroundColor: ['fill': '#E6E6E6', 'opacity': 100]
 	 }	
 }
