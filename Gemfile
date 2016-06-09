eda309fd3d8a1f72df96466f590efafe55f02ced
Ted Beaney <tedbeaney@hotmail.co.uk>
2016-06-09 08:21:25 +0100

    sms service re storage auth and retrieval auth

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
 gem 'recaptcha', require: 'recaptcha/rails'
 
+gem 'twilio-ruby'
+ 	
 # Use Unicorn as the app server
 # gem 'unicorn'
 
--- app/controllers/storages_controller.rb
+++ app/controllers/storages_controller.rb
@@ -1,65 +1,66 @@
 class StoragesController < ApplicationController
 
-  def new
+    def new
 
-    @keys = current_user.keys.where("location_id IS NULL")
+        @keys = current_user.keys.where("location_id IS NULL")
 
-    @storage = Storage.new
-
-  end
-
-  def create
-
-    @storage = Storage.new
-
-    location_id = pick_location
-
-    @storage.location_id = location_id
-    @key = Key.find_by id: params[:storage][:key_id]
-    @storage.key_id = @key.id
-
-    @key.auth_code = generateAuthcode
-    @key.location_id = location_id
-
-    @key.save
-
-
-    if @storage.save
-
-      flash['notice'] = "Storage information has been sent to you."
-      redirect_to root_path
-
-    else
-
-      flash['error'] = "Key storage has failed."
-      render new
+        @storage = Storage.new
 
     end
 
-  end
+    def create
 
-  private
+        @storage = Storage.new
 
-  def generateAuthcode
+        location_id = pick_location
 
-    psrn = Random.new
+        @storage.location_id = location_id
+        @key = Key.find_by id: params[:storage][:key_id]
+        @storage.key_id = @key.id
 
-    genAuth = psrn.rand(10000..99999)
+        @key.auth_code = generateAuthcode
+        @key.location_id = location_id
 
-    return genAuth
+        @key.save
 
-  end
 
-  private
+        if @storage.save
+            # send the user a text message
+            send_storage_sms(current_user.mobile_number, @storage, @key.auth_code)
 
-  def pick_location
+            # put a success flash message on the page and redirect
+            flash['notice'] = "Storage information has been sent to you."
+            redirect_to root_path
+        else
+            flash['error'] = "Key storage has failed."
+            render new
 
-    @locations = Location.all
+        end
 
-    @location = Location.order("RAND()").limit(1).first
+    end
 
-    return @location.id
+    private
 
-  end
+    def generateAuthcode
 
-end
+        psrn = Random.new
+
+        genAuth = psrn.rand(10000..99999)
+
+        return genAuth
+
+    end
+
+    private
+
+        def pick_location
+
+            @locations = Location.all
+
+            @location = Location.order("RAND()").limit(1).first
+
+            return @location.id
+
+        end
+
+    end
