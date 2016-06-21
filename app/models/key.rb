b3a74a582196e24fa543daa10f17d5aa1ef026ad
Perry Marquer <pezmo82@gmail.com>
2016-06-21 19:58:00 +0100

    Get key states

--- app/models/key.rb
+++ app/models/key.rb
@@ -3,4 +3,26 @@ class Key < ActiveRecord::Base
 	belongs_to :location
   	has_many :storages
   	has_many :retrievals
+
+  	def get_state
+  		storage = self.storages.first
+
+  		logger.debug storage.is_stored
+
+  		if !storage
+  			return "Unstored"
+  		elsif storage.to_drop
+  			return "Drop to location"
+  		elsif storage.is_stored
+  			return "Key is stored"
+  		elsif storage.to_collect
+  			return "Collect from location"
+  		elsif storage.was_retrieved
+  			return "Retrieved"
+  		else 
+  			return "unknown"
+  		end
+
+  	end
+
 end
