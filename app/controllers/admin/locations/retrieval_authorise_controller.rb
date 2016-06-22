5678d9a879e22717ece2b087230d2962a77b1828
Ted Beaney <tedbeaney@hotmail.co.uk>
2016-06-22 09:53:53 +0100

    correction to amendment of retrieval controller

--- app/controllers/admin/locations/retrieval_authorise_controller.rb
+++ app/controllers/admin/locations/retrieval_authorise_controller.rb
@@ -13,10 +13,10 @@ class Admin::Locations::RetrievalAuthoriseController < ApplicationController
 
         @keys.each do | key |
             if key.auth_code = params[admin_locations_retrieval_authorise_path][:authcode]
-                @storage = @key.storages.first
+                @storage = Storage.where("key_id = :key_id AND location_id = :location_id", {key_id: key.id, location_id: params[:id]}).order(is_stored: :asc).first
                 @storage.to_collect = false
                 @storage.was_retrieved = true
-                @retrieval = @key.retrievals.first
+                @retrieval = Retrieval.where("key_id = :key_id AND location_id = :location_id", {key_id: key.id, location_id: params[:id]}).order(was_retrieved: :asc).first
                 @retrieval.to_collect = false
                 @retrieval.was_retrieved = true
                 key.auth_code = nil
