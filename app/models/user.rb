00eb89f631802c443a49857c4f973ccdd63cd553
Perry Marquer <pezmo82@gmail.com>
2016-06-22 20:52:49 +0100

    Tidy up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,16 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    has_many :keys
+    belongs_to :rank
+    
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
-  validates :email, confirmation: true
-  validates :mobile_number, confirmation: true, presence: true
+    validates :email, confirmation: true
+    validates :mobile_number, confirmation: true, presence: true
 
-  has_many :keys
-  belongs_to :rank
-
-  private
-  def confirmation_token
-  	if self.confirm_token.blank?
-  		self.confirm_token = SecureRandom.urlsafe_base64.to_s
-  	end
-  end
+        private
+            def confirmation_token
+                if self.confirm_token.blank?
+                    self.confirm_token = SecureRandom.urlsafe_base64.to_s
+                end
+            end
 end
